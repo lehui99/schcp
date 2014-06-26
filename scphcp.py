50505f16075910d53a3bd3208e6ab5fbcaef0642
lehui99 <lehui99@gmail.com>
2014-06-26 16:36:56 +0800

    local functions

--- scphcp.py
+++ scphcp.py
@@ -50,81 +50,101 @@ class Tunnel(Pipe):
         self.config = config
         self.client = client
 
-    def recvFully(sock, byteslen):
-        buf = b''
-        while byteslen != 0:
-            t += sock.recv(byteslen)
-            if t == b'':
-                raise Exception('End connection in socksHandshake')
-            buf += t
-            byteslen -= len(t)
-        return buf
-
-    def socksHandshake(self):
-        raise Exception('Not implemented yet')  #   TODO:
-
-    def connectHandshake(self):
-        def recvHeaderFully(sock):
-            endBytes = b'\r\n\r\n'
-            idx = 0
-            buf = b''
-            while True:
-                t = self.client.recv(65536)
-                if t == b'':
-                    raise Exception('End connection in connectHandshake')
-                try:
-                    idx = buf.index(endBytes, idx)
-                    idx += 4
-                    return buf[ : idx], buf[idx : ]
-                except ValueError:
-                    idx = len(buf) - len(endBytes) + 1
-        request, self.clientData = recvHeaderFully(self.client)
-        method, host, protocol = re.split(b'\\s+', request[ : request.index(b'\r\n')])    #   Although not meets RFC, it's no matter.  Because it's a local proxy.
-        if method.upper() != b'CONNECT':
-            raise Exception('Not a CONNECT(HTTPS) proxy.')
-        self.hostname, self.port = host.split(b':')
-        self.port = int(self.port)
-        self.parent.send(request)
-        response, self.parentData = recvHeaderFully(self.parent)
-        self.client.send(response)
-
-    def startPipe(self):
-        if self.clientData != b'':
-            self.parent.send(self.clientData)
-        if self.parentData != b'':
-            self.client.send(self.parentData)
-        pipe = Pipe()
-        pipe.setSockPair(client, parent)
-        pipe.start()
-        self.setSockPair(parent, client)
-        self.pipeData()
-
-    def sslHandshake(self):
-        raise Exception('Not implemented yet')  #   TODO:
-
     def run(self):
+
+        def recvFully(sock, byteslen):
+            buf = b''
+            while byteslen != 0:
+                t += sock.recv(byteslen)
+                if t == b'':
+                    raise Exception('End connection in socksHandshake')
+                buf += t
+                byteslen -= len(t)
+            return buf
+
+        def socksHandshake():
+            raise Exception('Not implemented yet')  #   TODO:
+
+        def connectHandshake():
+
+            def recvHeaderFully(sock):
+                endBytes = b'\r\n\r\n'
+                idx = 0
+                buf = b''
+                while True:
+                    t = self.client.recv(65536)
+                    if t == b'':
+                        raise Exception('End connection in connectHandshake')
+                    try:
+                        idx = buf.index(endBytes, idx)
+                        idx += 4
+                        return buf[ : idx], buf[idx : ]
+                    except ValueError:
+                        idx = len(buf) - len(endBytes) + 1
+
+            request, clientData = recvHeaderFully(self.client)
+            method, host, protocol = re.split(b'\\s+', request[ : request.index(b'\r\n')])    #   Although not meets RFC, it's no matter.  Because it's a local proxy.
+            if method.upper() != b'CONNECT':
+                raise Exception('Not a CONNECT(HTTPS) proxy.')
+            self.hostname, self.port = host.split(b':')
+            self.port = int(self.port)
+            self.parent.send(request)
+            response, parentData = recvHeaderFully(self.parent)
+            self.client.send(response)
+            return clientData, parentData
+
+        def startPipe():
+            if clientData != b'':
+                self.parent.send(clientData)
+            if parentData != b'':
+                self.client.send(parentData)
+            pipe = Pipe()
+            pipe.setSockPair(client, parent)
+            pipe.start()
+            self.setSockPair(parent, client)
+            self.pipeData()
+
+        def sslHandshake():
+
+            def pipeHello(paramList):
+                sockIn, inData, sockOut = paramList
+                missDataLen = 5 - len(inData)
+                if missDataLen > 0:
+                    inData += recvFully(sockIn, missDataLen)
+                helloLen = ord(inData[3]) << 8 | ord(inData[4])
+                missDataLen = 5 + helloLen - len(inData)
+                if missDataLen > 0:
+                    inData += recvFully(sockIn, missDataLen)
+                packetLen = 5 + helloLen
+                sockOut.send(inData[ : packetLen])
+                return inData[packetLen : ]
+
+            clientData = pipeHello(self.client, clientData, self.parent)
+            parentData = pipeHello(self.parent, self,parentData, self,client)
+            raise Exception('Not implemented yet')  #   TODO:
+
         self.parent = socket.socket()
         try:
             if self.config['parentProxyHost'] != None:
                 self.parent.connect((self.config['parentProxyHost'], self.config['parentProxyPort']))
             if self.config['parentProxyType'] == ProxyType.NONE:
                 raise Exception('Not implemented yet')  #   TODO: Implement as local socks5 proxy
-                self.clientData = b''
-                self.parentData = b''
+                clientData = b''
+                parentData = b''
             elif self.config['parentProxyType'] == ProxyType.SOCKS:
-                self.socksHandshake()
-                self.clientData = b''
-                self.parentData = b''
+                socksHandshake()
+                clientData = b''
+                parentData = b''
             elif self.config['parentProxyType'] == ProxyType.CONNECT:
-                self.connectHandshake()
+                clientData, parentData = connectHandshake()
             else:
                 assert(False)
-            if self.clientData == b'':
-                self.clientData = self.client.recv(65536)
-            if self.clientData[0] != '\x22':    #   Not SSL
-                self.startPipe()
-            self.sslHandshake()
-            self.startPipe()
+            if clientData == b'':
+                clientData = self.client.recv(65536)
+            if clientData[0] != '\x22':    #   Not SSL
+                startPipe()
+            sslHandshake()
+            startPipe()
         except Exception:
             logging.exception('Exception in Tunnel.run:')
         finally:
