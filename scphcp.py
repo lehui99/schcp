2128f2c12734e2532f01491005a1d641a34b0b3e
lehui99 <lehui99@gmail.com>
2014-06-26 16:06:13 +0800

    add none proxy type implement pipe

--- scphcp.py
+++ scphcp.py
@@ -11,25 +11,57 @@ import sys
 import re
 
 class ProxyType:
+    NONE = 0
     SOCKS = 1
     CONNECT = 2
 
-class Tunnel(Concurrent):
+class Pipe(Concurrent):
+
+    def __init__(self):
+        Concurrent.__init__(self)
+
+    def setSockPair(self, sockIn, sockOut):
+        self.sockIn = sockIn
+        self.sockOut = sockOut
+
+    def pipeData(self):
+        try:
+            while True:
+                data = self.sockIn.recv(65536)
+                if data == b'' or data == '' or data == None:
+                    break
+                self.sockOut.send(data)
+        except Exception:
+            logging.info('Pipe end')
+        finally:
+            self.sockIn.close()
+            self.sockOut.close()
+
+    def run(self):
+        self.pipeData()
+
+    def _run(self):
+        self.run()
+
+class Tunnel(Pipe):
 
     def __init__(self, config, client):
+        Pipe.__init__(self)
         self.config = config
         self.client = client
 
+    def recvFully(sock, byteslen):
+        buf = b''
+        while byteslen != 0:
+            t += sock.recv(byteslen)
+            if t == b'':
+                raise Exception('End connection in socksHandshake')
+            buf += t
+            byteslen -= len(t)
+        return buf
+
     def socksHandshake(self):
-        def recvFully(sock, byteslen):
-            buf = b''
-            while byteslen != 0:
-                t += sock.recv(byteslen)
-                if t == b'':
-                    raise Exception('End connection in socksHandshake')
-                buf += t
-                byteslen -= len(t)
-            return buf
+        raise Exception('Not implemented yet')  #   TODO:
 
     def connectHandshake(self):
         def recvHeaderFully(sock):
@@ -47,7 +79,7 @@ class Tunnel(Concurrent):
                 except ValueError:
                     idx = len(buf) - len(endBytes) + 1
         request, self.clientData = recvHeaderFully(self.client)
-        method, host, protocol = re.split(b'\\s+', request[ : request.index(b'\r\n')])    #   Although not met RFC, it's no matter.  Because it's a local proxy.
+        method, host, protocol = re.split(b'\\s+', request[ : request.index(b'\r\n')])    #   Although not meets RFC, it's no matter.  Because it's a local proxy.
         if method.upper() != b'CONNECT':
             raise Exception('Not a CONNECT(HTTPS) proxy.')
         self.hostname, self.port = host.split(b':')
@@ -56,31 +88,57 @@ class Tunnel(Concurrent):
         response, self.parentData = recvHeaderFully(self.parent)
         self.client.send(response)
 
+    def startPipe(self):
+        if self.clientData != b'':
+            self.parent.send(self.clientData)
+        if self.parentData != b'':
+            self.client.send(self.parentData)
+        pipe = Pipe()
+        pipe.setSockPair(client, parent)
+        pipe.start()
+        self.setSockPair(parent, client)
+        self.pipeData()
+
+    def sslHandshake(self):
+        raise Exception('Not implemented yet')  #   TODO:
+
     def run(self):
         self.parent = socket.socket()
         try:
-            self.parent.connect((self.config['parentProxyHost'], self.config['parentProxyPort']))
-            if self.config['parentProxyType'] == ProxyType.SOCKS:
+            if self.config['parentProxyHost'] != None:
+                self.parent.connect((self.config['parentProxyHost'], self.config['parentProxyPort']))
+            if self.config['parentProxyType'] == ProxyType.NONE:
+                raise Exception('Not implemented yet')  #   TODO: Implement as local socks5 proxy
+                self.clientData = b''
+                self.parentData = b''
+            elif self.config['parentProxyType'] == ProxyType.SOCKS:
                 self.socksHandshake()
+                self.clientData = b''
+                self.parentData = b''
             elif self.config['parentProxyType'] == ProxyType.CONNECT:
                 self.connectHandshake()
             else:
                 assert(False)
+            if self.clientData == b'':
+                self.clientData = self.client.recv(65536)
+            if self.clientData[0] != '\x22':    #   Not SSL
+                self.startPipe()
+            self.sslHandshake()
+            self.startPipe()
         except Exception:
             logging.exception('Exception in Tunnel.run:')
         finally:
             self.client.close()
             self.parent.close()
 
-    def _run(self):
-        self.run()
-
 class Main(object):
 
     def __init__(self, config):
         logging.basicConfig(filename = config['logFilename'], level = getattr(logging, config['logLevel']))
         config['parentProxyType'] = config['parentProxyType'].lower()
-        if config['parentProxyType'] == 'socks':
+        if config['parentProxyType'] == 'none':
+            config['parentProxyType'] = ProxyType.NONE
+        elif config['parentProxyType'] == 'socks':
             config['parentProxyType'] = ProxyType.SOCKS
         elif config['parentProxyType'] == 'socks':
             config['parentProxyType'] = ProxyType.CONNECT
